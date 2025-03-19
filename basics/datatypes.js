5e13aaa299cdbeb188e40f52b903606c4cf4595c
AparnaGIT25 <raia6254@gmail.com>
2025-03-19 18:49:04 +0000

    datatypes added

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+//alert(3+3) // we are using node js ,not browser
+let name="aparna";
+let age=21;
+let isloggedin=false;
+
+/*
+there are various kinds of datatypes available to use some are:
+number=> 2 to power 53
+bigint
+string=""
+boolean
+null=>representation of an empty value
+undefined=>value is not assigned yet
+symbol=>used to find uniqueness
+
+
+*/
+console.log(typeof("age"))
