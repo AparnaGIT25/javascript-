de3c4bceb4850978ddf9fc3f0aff90995b3ae1f0
AparnaGIT25 <raia6254@gmail.com>
2025-03-20 11:41:51 +0000

    summary added

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+//primitive (call by value)
+//7 types:string,number,boolean,null,undefined,Symbol(used for making unique),BigInt
+
+//Non-primitive type(reference type)
+// Array, Objects, Functions
+
+//javascript is a dynamically typed language as data type will automatically be assigned at the time of compilation of code execution//
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       */
