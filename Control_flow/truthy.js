97116abb01fba09539ccc1ed9dee2ab4b1055bfb
AparnaGIT25 <raia6254@gmail.com>
2025-03-25 20:27:22 +0000

    changes added

--- Control_flow/truthy.js
+++ Control_flow/truthy.js
@@ -26,3 +26,24 @@ const emptyObj={}
 if(Object.keys(emptyObj)===0){
     console.log("object is empty")
 }
+/*
+Nullish Coalescing Operator (??) in JavaScript
+The nullish coalescing operator (??) is used to provide a default value when a variable is null or undefined. It does not consider false, 0, or "" as nullish.
+*/
+//acts as safety check for "null"
+let val1;
+//val1= 5 ?? 10
+//val1= null ?? 10
+val1= undefined ?? 15
+console.log(val1)// o/p->15
+
+//TERNIARY OPERATOR
+/*The ternary operator is a shorthand for if...else statements. 
+It is used to evaluate a condition and return one of two values based on whether the condition is true or false.
+*/
+//syn->
+//condition ? expression_if_true : expression_if_false;
+const iceTeaPrice=100;
+iceTeaPrice>=80? console.log("you can buy"):console.log("do not buy it")
+
+
